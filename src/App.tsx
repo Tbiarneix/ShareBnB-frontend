01386e6360b8e9a9a6f4a90fba5c3b1999ad3b31
Thomas Biarneix <thomas.biarneix@voltyo.com>
2022-07-04 21:42:12 +0200

    Home initialisation without Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { useState } from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 // Components
-// import Home from './components/home/Home';
+import Home from './components/home/Home';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to React Router!</h1>
+    <div>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/home/Home.jsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-  <div>
-    <h1>Welcome</h1>
-  </div>
-}
-
-export default Home;
